399745b3dd8402551529f476deab7cd723c2abfd
kirillmasich <39246532+kirillmasich@users.noreply.github.com>
2019-09-25 18:56:51 +0300

    update

--- JSGames/revers/revers.js
+++ JSGames/revers/revers.js
@@ -1,14 +1,10 @@
-var soundFile, reverb;
+var drum;
+
 function preload() {
-  soundFile = loadSound("sound.mp3");
+  drum = loadSound('sound.mp3');
 }
 
 function setup() {
-  reverb = new p5.Reverb();
-  soundFile.disconnect(); // so we'll only hear reverb...
-
-  // connect soundFile to reverb, process w/
-  // 3 second reverbTime, decayRate of 2%
-  reverb.process(soundFile, 3, 2);
-  soundFile.play();
+  drum.reverseBuffer();
+  drum.play();
 }
